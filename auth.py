8e642c362e9644504230819d8451fd088d1de92c
cdukcom <cduqueacero2016@gmail.com>
2025-07-08 16:52:24 -0500

    fix: use JSON Firebase credentials

--- auth.py
+++ auth.py
@@ -4,13 +4,14 @@ import os
 import json
 
 if not firebase_admin._apps:
-    credentials_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-    if not credentials_path:
-        raise Exception("GOOGLE_APPLICATION_CREDENTIALS not set")
+    json_cred = os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON")
+    if not json_cred:
+        raise Exception("GOOGLE_APPLICATION_CREDENTIALS_JSON not set")
 
-    cred = credentials.Certificate(credentials_path)
+    cred_dict = json.loads(json_cred)
+    cred = credentials.Certificate(cred_dict)
     firebase_admin.initialize_app(cred)
-
+    
 def verify_token(id_token: str):
     try:
         decoded_token = auth.verify_id_token(id_token)
