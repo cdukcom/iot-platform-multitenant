e5390a2c1110d4b09a00f7cdb90ce0dae7ceb11f
cdukcom <cduqueacero2016@gmail.com>
2025-07-31 12:25:59 -0500

    🔧 Regenerados archivos gRPC correctamente desde .proto

--- chirpstack_proto/device_profile/device_profile_pb2.py
+++ chirpstack_proto/device_profile/device_profile_pb2.py
@@ -8,8 +8,6 @@ from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import runtime_version as _runtime_version
 from google.protobuf import symbol_database as _symbol_database
-from google.protobuf import message
-from google.protobuf import descriptor_pb2
 from google.protobuf.internal import builder as _builder
 _runtime_version.ValidateProtobufRuntimeVersion(
     _runtime_version.Domain.PUBLIC,
@@ -26,42 +24,22 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n#device_profile/device_profile.proto\x12\x0e\x64\x65vice_profile\"%\n\x17GetDeviceProfileRequest\x12\n\n\x02id\x18\x01 \x01(\tb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n#device_profile/device_profile.proto\x12\x0e\x64\x65vice_profile\"%\n\x17GetDeviceProfileRequest\x12\n\n\x02id\x18\x01 \x01(\t\"9\n\rDeviceProfile\x12\n\n\x02id\x18\x01 \x01(\t\x12\x0c\n\x04name\x18\x02 \x01(\t\x12\x0e\n\x06region\x18\x03 \x01(\t\"]\n\x19ListDeviceProfilesRequest\x12\r\n\x05limit\x18\x01 \x01(\r\x12\x0e\n\x06offset\x18\x02 \x01(\r\x12\x0e\n\x06search\x18\x03 \x01(\t\x12\x11\n\ttenant_id\x18\x04 \x01(\t\"`\n\x1aListDeviceProfilesResponse\x12\x13\n\x0btotal_count\x18\x01 \x01(\r\x12-\n\x06result\x18\x02 \x03(\x0b\x32\x1d.device_profile.DeviceProfile2\xc4\x01\n\x14\x44\x65viceProfileService\x12M\n\x03Get\x12\'.device_profile.GetDeviceProfileRequest\x1a\x1d.device_profile.DeviceProfile\x12]\n\x04List\x12).device_profile.ListDeviceProfilesRequest\x1a*.device_profile.ListDeviceProfilesResponseB8Z6github.com/chirpstack/chirpstack/api/go/device_profileb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'device_profile.device_profile_pb2', _globals)
 if not _descriptor._USE_C_DESCRIPTORS:
-  DESCRIPTOR._loaded_options = None
+  _globals['DESCRIPTOR']._loaded_options = None
+  _globals['DESCRIPTOR']._serialized_options = b'Z6github.com/chirpstack/chirpstack/api/go/device_profile'
   _globals['_GETDEVICEPROFILEREQUEST']._serialized_start=55
   _globals['_GETDEVICEPROFILEREQUEST']._serialized_end=92
+  _globals['_DEVICEPROFILE']._serialized_start=94
+  _globals['_DEVICEPROFILE']._serialized_end=151
+  _globals['_LISTDEVICEPROFILESREQUEST']._serialized_start=153
+  _globals['_LISTDEVICEPROFILESREQUEST']._serialized_end=246
+  _globals['_LISTDEVICEPROFILESRESPONSE']._serialized_start=248
+  _globals['_LISTDEVICEPROFILESRESPONSE']._serialized_end=344
+  _globals['_DEVICEPROFILESERVICE']._serialized_start=347
+  _globals['_DEVICEPROFILESERVICE']._serialized_end=543
 # @@protoc_insertion_point(module_scope)
-
-
-class DeviceProfile(message.Message):
-    __slots__ = ()
-    
-    DESCRIPTOR = descriptor_pb2.DescriptorProto(
-        name='DeviceProfile',
-        field=[
-            descriptor_pb2.FieldDescriptorProto(
-                name='id',
-                number=1,
-                label=descriptor_pb2.FieldDescriptorProto.LABEL_OPTIONAL,
-                type=descriptor_pb2.FieldDescriptorProto.TYPE_STRING,
-            ),
-            descriptor_pb2.FieldDescriptorProto(
-                name='name',
-                number=2,
-                label=descriptor_pb2.FieldDescriptorProto.LABEL_OPTIONAL,
-                type=descriptor_pb2.FieldDescriptorProto.TYPE_STRING,
-            ),
-            descriptor_pb2.FieldDescriptorProto(
-                name='region',
-                number=3,
-                label=descriptor_pb2.FieldDescriptorProto.LABEL_OPTIONAL,
-                type=descriptor_pb2.FieldDescriptorProto.TYPE_STRING,
-            ),
-        ]
-    )
-
